1421e80cb0765ea30ad257e4d7db9faa9f056128
C.Ponapalt <ponapalt@shillest.net>
2021-07-11 20:03:13 +0900

    adjust coding style and compiler compatibility

--- make_aya.bat
+++ make_aya.bat
@@ -9,6 +9,8 @@ copy /B /Y .\Release\aya.dll tmp\aya\aya.dll
 copy /B /Y .\readme.txt tmp\aya\readme.txt
 copy /B /Y .\readme-original.txt tmp\aya\readme-original.txt
 copy /B /Y .\messagetxt\*.* tmp\aya\messagetxt\
+del /F /S /Q tmp\aya\*.bak
+del /F /S /Q tmp\aya\messagetxt\*.bak
 
 del /F /S /Q tmp\aya.zip
 
