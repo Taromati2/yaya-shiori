d9dc3f085dac35eeeac46d300766689e5af4aabc
steve green <steve_green@qq.com>
2021-11-23 22:48:43 +0800

    std::int64_t

--- sysfunc.cpp
+++ sysfunc.cpp
@@ -3980,7 +3980,7 @@ CValue	CSystemFunction::GETTICKCOUNT(CSF_FUNCPARAM &p)
 	}
 
 #if defined(WIN32)
-	typedef aya::uint64 (WINAPI *DefGetTickCount64)();
+	typedef std::uint64_t (WINAPI *DefGetTickCount64)();
 
 	DefGetTickCount64 pGetTickCount64 = (DefGetTickCount64)::GetProcAddress(::GetModuleHandle("kernel32"),"GetTickCount64");
 
