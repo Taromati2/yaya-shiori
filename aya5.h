c8bb2c85f0c0fb06cbb829467c44a7e241bbd301
steve02081504 <steve02081504@EDEN.fukwold>
2022-07-11 16:08:53 +0800

    remove multi

--- aya5.h
+++ aya5.h
@@ -35,10 +35,6 @@ extern "C" DLLEXPORT BOOL_TYPE FUNCATTRIB load(aya::global_t h, long len);
 extern "C" DLLEXPORT BOOL_TYPE FUNCATTRIB unload();
 extern "C" DLLEXPORT aya::global_t FUNCATTRIB request(aya::global_t h, long *len);
 
-extern "C" DLLEXPORT long FUNCATTRIB multi_load(aya::global_t h, long len);
-extern "C" DLLEXPORT BOOL_TYPE FUNCATTRIB multi_unload(long id);
-extern "C" DLLEXPORT aya::global_t FUNCATTRIB multi_request(long id,aya::global_t h, long *len);
-
 #if defined(WIN32)
 extern "C" DLLEXPORT BOOL_TYPE FUNCATTRIB logsend(long hwnd);
 #endif
